fde7fccf139b7596329845622dbfb0da6efa92a3
Johan Andersson <johanstechs@gmail.com>
2022-06-08 23:10:22 +0200

    Finished section 1, getting started

---
+++ 1-GettingStarted/app.js
@@ -0,0 +1,33 @@
+//? Vanilla JS
+// const buttonEl = document.querySelector("button");
+// const inputEl = document.querySelector("input");
+// const listEl = document.querySelector("ul");
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement("li");
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = "";
+// }
+
+// buttonEl.addEventListener("click", addGoal);
+
+//? Vue.js
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      if (this.enteredValue === "") {
+        return;
+      }
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
